3f45da2dbfa46973fbf13d5466b40a2ad760b950
Alan Barzanti - alan.barzanti@studio.unibo.it <alan.barzanti@studio.unibo.it>
2024-12-09 12:21:41 +0100

    Done first exercise

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,59 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LogicsImpl implements Logics {
 
+	private final int size;
+	private final List<Integer> values;
+	private final List<Boolean> enablings = new ArrayList<>();
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		this.size = size;
+		values = new ArrayList<>();
+		for(int i = 0; i < this.size; i++) this.values.add(0);
+		for(int i = 0; i < this.size; i++) this.enablings.add(true);
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.size;
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return new ArrayList<>(values);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return new ArrayList<>(enablings);
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		int val = this.values.get(elem) + 1;
+		this.values.set(elem, val);
+		if (val == this.size) {
+			this.enablings.set(elem, false);
+		}
+		return val;
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		String result = this.values.toString().replace("[", "<<")
+		.replace("]", ">>").replaceAll(", ", "|");
+		return result;
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		int tmp = this.values.get(0);
+		for (var e : this.values) {
+			if (e != tmp) return false;
+		}
+		return true;
 	}
 }
